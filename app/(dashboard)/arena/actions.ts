83a6ac18e6591271c1cb09ff707bbb194d6dfdf5
Cong Nguyen <cong.nguyen@sioux.asia>
2026-02-26 21:44:06 +0700

    Remove unused import

--- app/(dashboard)/arena/actions.ts
+++ app/(dashboard)/arena/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { ArenaCategory, ArenaEventStatus, Role } from "@prisma/client";
+import { ArenaEventStatus, Role } from "@prisma/client";
 import { getServerSession } from "next-auth";
 import { revalidatePath } from "next/cache";
 
